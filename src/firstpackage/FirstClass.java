8545cdcbb4231a881dba951c885adc6923215054
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2020-06-20 15:44:30 -0400

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 
 	
 
-	System.out.println("conflict yapmak istiyoruz");
+	System.out.println("we are against the conflicts");
 
 	System.out.println("first commit from remote repository");
 	
